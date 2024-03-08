95ae4b868c4632e247db5e01117a06935092b091
mohamed mohi <mohimm00@gmail.com>
2024-03-08 03:04:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    const form = document.getElementById('nameForm');
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+        const name = document.getElementById('nameInput').value;
+        
+        // Check if the name arbic or not exists (Valdtion)
+        if (!/^[أ-ي\s]+$/g.test(name)) {
+            displayMessage('مش هينفع والله يابيه، اكتب اسمك بالعربي 🚫', true);
+        } else {
+            displayMessage(`كل سنة وأنت طيب وبخير وينعاد عليك باليمن والبركات يا ${name} 🎉`);
+        }
+    });
+};
+
+function displayMessage(message, isError = false) {
+    const greetingMessage = document.getElementById('greetingMessage');
+    greetingMessage.innerHTML = message;
+    greetingMessage.style.color = isError ? 'red' : ''; 
+    greetingMessage.classList.remove("messageEffect"); 
+    greetingMessage.classList.add("messageEffectFadeInScale"); 
+}
